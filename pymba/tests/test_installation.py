0a4871e34fdae93c34bca9dc39871ebe43d22ea8
Matthias Blaicher <matthias.blaicher@kit.edu>
2016-02-09 22:45:44 +0100

    Initial python 3 support

--- pymba/tests/test_installation.py
+++ pymba/tests/test_installation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+from __future__ import absolute_import, print_function, division
 from pymba import Vimba
 
 
--- pymba/vimbaexception.py
+++ pymba/vimbaexception.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import exceptions
 
 
 class VimbaException(Exception):
