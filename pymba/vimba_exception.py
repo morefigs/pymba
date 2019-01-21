fc5bd5ed0009226bfd8d33731e7102e7fdf8b7a3
morefigs <morefigs@gmail.com>
2019-01-21 15:11:46 +1100

    update setup

--- pymba/vimba_exception.py
+++ pymba/vimba_exception.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 class VimbaException(Exception):
 
     """
--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 
 setup(name='pymba',
       version=0.1,
-      description='pymba is a Python wrapper for the Allied Vision Technologies (AVT) Vimba C API.',
-      long_description='pymba is a Python wrapper for the Allied Vision Technologies (AVT) Vimba C API. It wraps the '
-                       'VimbaC.dll file included in the AVT Vimba installation to provide a simple Python interface '
-                       'for AVT cameras. It currently supports most of the functionality provided by VimbaC.dll.',
+      description="Pymba is a Python wrapper for Allied Vision's Vimba SDK.",
+      long_description="Pymba is a Python wrapper for Allied Vision's Vimba SDK. It uses the Vimba C API included in "
+                       "the Allied Vision Vimba installation to provide a simple Python interface for Allied Vimba "
+                       "cameras.",
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'Intended Audience :: Science/Research',
           'Intended Audience :: Manufacturing',
-          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: GPL-3.0 License',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX :: Linux',
           'Programming Language :: Python',
@@ -20,13 +20,11 @@ setup(name='pymba',
           'Topic :: Multimedia :: Graphics :: Graphics Conversion',
           'Topic :: Scientific/Engineering :: Image Recognition',
           'Topic :: Software Development :: Libraries :: Python Modules'],
-      keywords='python, opencv, cv, machine vision, computer vision, image recognition, vimba,'
-               'allied vision technologies, avt',
+      keywords='python, opencv, machine vision, computer vision, image recognition, vimba, vimba-sdk, allied vision',
       author='morefigs',
       author_email='morefigs@gmail.com',
       url='https://github.com/morefigs/pymba',
-      license='MIT',
-      packages=['pymba', 'tests'],
+      license='GPL-3.0',
+      packages=['pymba'],
       zip_safe=False,
-      requires=['numpy'],
-      )
+      requires=['numpy'])
