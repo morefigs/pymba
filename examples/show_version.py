810b1f0a061988b451f9d425d8788596ae39118b
morefigs <morefigs@gmail.com>
2019-01-30 14:28:00 +1100

    update version example

--- examples/show_version.py
+++ examples/show_version.py
@@ -3,6 +3,4 @@ from pymba import Vimba, PYMBA_VERSION
 
 if __name__ == '__main__':
     print(f'Pymba version: {PYMBA_VERSION}')
-
-    vimba = Vimba()
-    print(f'Vimba version: {vimba.version}')
+    print(f'Vimba version: {Vimba.version()}')
