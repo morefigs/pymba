6d6fc471aceff5802fc3ad5db3d35521e87abf4c
morefigs <morefigs@gmail.com>
2019-01-30 13:59:23 +1100

    delete outdates tests

--- tests/test_installation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from __future__ import absolute_import, print_function, division
-from pymba import Vimba
-
-
-def test_installation():
-    with Vimba() as vimba:
-	    version = vimba.getVersion()
-	    assert version == '1.2.0'
