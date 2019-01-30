3915eff02c414ad13c065cf7536be95063fa7673
morefigs <morefigs@gmail.com>
2019-01-30 15:56:45 +1100

    added upload hint

--- setup.py
+++ setup.py
@@ -51,3 +51,7 @@ setup(name='Pymba',
           ]
       }
       )
+
+# python3 -m pip install --user --upgrade setuptools wheel twine
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload dist/*
