9066b9458872b44be8df4563a13858e10d712f05
morefigs <morefigs@gmail.com>
2019-04-13 15:54:36 +1000

    Catch timeout in single frame acquisition example

--- examples/camera/opencv_acquire_image.py
+++ examples/camera/opencv_acquire_image.py
@@ -1,4 +1,4 @@
-from pymba import Vimba
+from pymba import Vimba, VimbaException
 from examples.camera.display_frame import display_frame
 
 
@@ -12,8 +12,17 @@ if __name__ == '__main__':
 
         # capture a single frame, more than once if desired
         for i in range(1):
-            frame = camera.acquire_frame()
-            display_frame(frame, 0)
+            try:
+                frame = camera.acquire_frame()
+                display_frame(frame, 0)
+            except VimbaException as e:
+                # rearm camera upon frame timeout
+                if e.error_code == VimbaException.ERR_TIMEOUT:
+                    print(e)
+                    camera.disarm()
+                    camera.arm('SingleFrame')
+                else:
+                    raise
 
         camera.disarm()
 
