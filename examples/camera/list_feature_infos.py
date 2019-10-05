df6f138c97c6979b63d68dba1bc9475b56138333
Gabri <g.abbate1@campus.unimib.it>
2019-10-05 12:05:45 +0200

    added load conf from file

--- examples/camera/list_feature_infos.py
+++ examples/camera/list_feature_infos.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     with Vimba() as vimba:
         camera = vimba.camera(0)
         camera.open()
-
+        camera.load_config(b'gabri.xml')
         for feature_name in camera.feature_names():
             feature = camera.feature(feature_name)
             print(feature.info)
